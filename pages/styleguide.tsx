133e513cd03c2eb05ef60c4a50271fa88cf0bf24
Sasa Stevcic <sasa.stevcic@yahoo.com>
2022-04-06 12:50:59 +0200

    Hide styleguide route in production

--- pages/styleguide.tsx
+++ pages/styleguide.tsx
@@ -1,6 +1,13 @@
 import type { NextPage } from 'next';
 import StyleguideTemplate from '../components/templates/Styleguide';
 
+export const getStaticProps = () => {
+	return {
+		props: {},
+		notFound: process.env.NODE_ENV === 'production',
+	};
+};
+
 const Styleguide: NextPage = () => {
 	return <StyleguideTemplate />;
 };

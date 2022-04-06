b10e4c19f0dd1bef29615ce73899aa2200ce35da
Sasa Stevcic <sasa.stevcic@yahoo.com>
2022-04-06 15:27:18 +0200

    Extend default props

--- components/templates/Home/Home.tsx
+++ components/templates/Home/Home.tsx
@@ -1,17 +1,29 @@
 import { ReactElement } from 'react';
 import { Path } from '../../../constants/path';
+import { useSWR } from '../../../hooks/useSWR';
+import { HelloData } from '../../../pages/api/hello';
 import Button from '../../atoms/Button';
 import Heading from '../../atoms/Heading';
+import Paragraph from '../../atoms/Paragraph';
+import { ParagraphType } from '../../atoms/Paragraph/Paragraph.data';
 import Container from '../../layout/Container';
 import Flex from '../../layout/Flex';
 import Page from '../../layout/Page';
 
 export const Home = (): ReactElement => {
+	const { data: hello, mutate } = useSWR<HelloData>('/hello');
+
 	return (
 		<Page>
 			<Container>
 				<Flex flexDirection="column" justifyContent="center" alignItems="center">
-					<Heading>Create Next App - Starter</Heading>
+					<Heading style={{ marginBottom: '1rem' }}>{hello?.title}</Heading>
+					<Paragraph type={ParagraphType.Large}>
+						Current time: <strong>{hello?.time}</strong>
+					</Paragraph>
+					<Button onClick={() => mutate()} style={{ marginBottom: '2rem' }}>
+						Revalidate Time
+					</Button>
 					<Button href={Path.Styleguide}>Styleguide</Button>
 				</Flex>
 			</Container>

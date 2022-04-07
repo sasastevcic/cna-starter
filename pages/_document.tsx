42e65dbfecd44caa0a54e44ba1fc7bf2ac82cea6
Sasa Stevcic <sasa.stevcic@yahoo.com>
2022-04-07 16:12:40 +0200

    Remove preloading fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,22 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 const MyDocument = () => {
 	return (
 		<Html>
-			<Head>
-				<link
-					rel="preload"
-					href="/fonts/Lato-Bold.woff2"
-					as="font"
-					type="font/woff2"
-					crossOrigin=""
-				/>
-				<link
-					rel="preload"
-					href="/fonts/Lato-Regular.woff2"
-					as="font"
-					type="font/woff2"
-					crossOrigin=""
-				/>
-			</Head>
+			<Head />
 			<body>
 				<Main />
 				<div id="modal" />

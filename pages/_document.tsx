66209f8c38fd21a1d08dadb5413a844ccef17687
Sasa Stevcic <sasa.stevcic@yahoo.com>
2022-04-08 09:33:33 +0200

    Add ServerStyleSheet from styled-components

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 const MyDocument = () => {
 	return (
@@ -14,3 +15,28 @@ const MyDocument = () => {
 };
 
 export default MyDocument;
+
+MyDocument.getInitialProps = async (ctx: DocumentContext): Promise<DocumentInitialProps> => {
+	const sheet = new ServerStyleSheet();
+	const originalRenderPage = ctx.renderPage;
+
+	try {
+		ctx.renderPage = () =>
+			originalRenderPage({
+				enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+			});
+
+		const initialProps = await MyDocument.getInitialProps(ctx);
+		return {
+			...initialProps,
+			styles: (
+				<>
+					{initialProps.styles}
+					{sheet.getStyleElement()}
+				</>
+			),
+		};
+	} finally {
+		sheet.seal();
+	}
+};
